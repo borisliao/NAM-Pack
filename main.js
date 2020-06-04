2bf177da1d7920a9ac68acbac498638e25c70c5f
Boris Liao <borisliaos@gmail.com>
2020-06-04 15:35:16 -0400

    added semicolons

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-const {app, BrowserWindow, Menu, ipcMain, dialog, shell} = require('electron')
+const {app, BrowserWindow, Menu, ipcMain, dialog, shell} = require('electron');
 const path = require('path');
 const {download} = require("electron-dl");
-const fs = require("fs")
+const fs = require("fs");
 const trash = require('trash');
 const {autoUpdater} = require("electron-updater");
 var extract = require('extract-zip');
 var request = require('request');
-const fse = require('fs-extra')
+const fse = require('fs-extra');
 
 let mainWindow;
 let loginWindow;
 
 function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-  mainWindow.loadFile('index.html')
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadFile('index.html');
 
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
   // Insert menu
@@ -21,7 +21,7 @@ function createWindow () {
 
   // Turn on dev tools if in test
   if(process.env.NODE_ENV == 'test'){
-    mainWindow.toggleDevTools()
+    mainWindow.toggleDevTools();
   }
 
   ipcMain.on("download", (event, info) => {
@@ -34,7 +34,7 @@ function createWindow () {
     mainWindow = null;
     loginWindow = null;
     app.quit();
-  })
+  });
 }
 
 function sendStatusToWindow(text) {
@@ -72,15 +72,15 @@ autoUpdater.on('update-downloaded', (info) => {
 });
 
 function logWindow () {
-  loginWindow = new BrowserWindow({width: 400, height: 200})
-  loginWindow.loadFile('login.html')
+  loginWindow = new BrowserWindow({width: 400, height: 200});
+  loginWindow.loadFile('login.html');
 
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
   Menu.setApplicationMenu(mainMenu);
 
   loginWindow.on('closed', function () {
     loginWindow = null;
-  })
+  });
 }
 
 function getFilenameFromUrl(url){
@@ -89,12 +89,12 @@ function getFilenameFromUrl(url){
 var callTimes = 0;
 
 ipcMain.on('vanillaNewpack',function(){
-  var modpackDir = path.join(app.getPath("userData"), "process", "MultiMC", "instances")
+  var modpackDir = path.join(app.getPath("userData"), "process", "MultiMC", "instances");
   var info = {
     url: "https://github.com/borisliao/nam-dist/releases/latest/download/vanilla.zip",
     properties: {directory: modpackDir}
   };
-  console.log("Dl")
+  console.log("Dl");
   var vanilla_dl = download(BrowserWindow.getFocusedWindow(), info.url, info.properties);
   vanilla_dl.then(dl => extract(path.join(modpackDir,"vanilla.zip"),{dir: modpackDir},function (err){
     if(err){
@@ -105,7 +105,7 @@ ipcMain.on('vanillaNewpack',function(){
 
 // download and process a new pack
 ipcMain.on('newpack',function(){
-  var modpackDir = path.join(app.getPath("userData"), "process", "MultiMC", "instances", "NAM Pack")
+  var modpackDir = path.join(app.getPath("userData"), "process", "MultiMC", "instances", "NAM Pack");
 
   // Download twitch pack
   var info = {
@@ -123,11 +123,11 @@ ipcMain.on('newpack',function(){
         });
       // Delete original mods folder and rebuild from manifest.json
       if(fs.existsSync(path.join(modpackDir,"minecraft","mods"))){
-        fs.unlink(path.join(modpackDir,"minecraft","mods"),function(err){console.error(err)});
+        fs.unlink(path.join(modpackDir,"minecraft","mods"),function(err){console.error(err);});
       }
       // Delete overrides folder
       if(fs.existsSync(path.join(modpackDir,"overrides"))){
-        fs.unlink(path.join(modpackDir,"overrides"),function(err){console.error(err)});
+        fs.unlink(path.join(modpackDir,"overrides"),function(err){console.error(err);});
       }
 
       if (!fs.existsSync(path.join(modpackDir,"minecraft"))){
@@ -141,7 +141,7 @@ ipcMain.on('newpack',function(){
           fileID = man.files[i].fileID;
           var r = request.get('https://addons-ecs.forgesvc.net/api/v2/addon/'+projectID+'/file/'+fileID+'/download-url', function (err, res, body) {
             request.get(body).pipe(fs.createWriteStream(path.join(modpackDir,"minecraft","mods",getFilenameFromUrl(body)))).on('finish', function(){
-                callTimes+=1
+                callTimes+=1;
                 mainWindow.webContents.executeJavaScript('App.state('+ callTimes.toString() +'/'+ man.files.length +')');
                 if(callTimes == man.files.length){
                   mainWindow.webContents.executeJavaScript('App.state("mod download finished!")');
@@ -170,22 +170,22 @@ ipcMain.on('credentials',function(e,item){
   console.log(item);
   mainWindow.webContents.send('credentials', item);
   loginWindow.close();
-})
+});
 
 // Hide window function
 ipcMain.on('hide',function(){
   mainWindow.hide();
-})
+});
 
 // Check for updates
 ipcMain.on('checkUpdate',function(){
   autoUpdater.checkForUpdates();
-})
+});
 
 // catch close call
 ipcMain.on('close',function(e){
   mainWindow.close();
-})
+});
 
 app.on('ready', function(){
   createWindow();
@@ -193,12 +193,12 @@ app.on('ready', function(){
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    app.quit()
-})
+    app.quit();
+});
 
 app.on('activate', function () {
-    createWindow()
-})
+    createWindow();
+});
 
 const mainMenuTemplate = [
   {
@@ -216,12 +216,12 @@ const mainMenuTemplate = [
           let options  = {
             buttons: ["Yes","Cancel"],
             message: "Do you really want to delete MultiMC? This sends your MultiMC to the trash!"
-          }
-          var cancel = dialog.showMessageBox(options)
+          };
+          var cancel = dialog.showMessageBox(options);
           if(!cancel){
             trash(path.join(app.getPath("userData"), "process")).then(function(){
               mainWindow.reload();
-            })
+            });
           }
         }
       },
@@ -234,7 +234,7 @@ const mainMenuTemplate = [
       {
         label:'Open process folder',
         click(){
-          shell.openItem(path.join(app.getPath("userData"), "process"))
+          shell.openItem(path.join(app.getPath("userData"), "process"));
         }
       },
       {
@@ -268,4 +268,4 @@ mainMenuTemplate.push({
       role: 'reload'
     }
   ]
-})
+});
