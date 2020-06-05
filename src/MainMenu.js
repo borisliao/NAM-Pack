3a33500aa977c783b33bc5c51b62e33b8a4998f8
Boris Liao <borisliaos@gmail.com>
2020-06-05 13:55:50 -0400

    added semicolon

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,29 +1,26 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
 const electron = require('electron');
 const {ipcRenderer} = electron;
-const app = require('electron').remote.app
-const fs = require('electron').remote.require('fs')
+const app = require('electron').remote.app;
+const fs = require('electron').remote.require('fs');
 var path = require('path');
 var extract = require('extract-zip');
 const {getCurrentWindow, globalShortcut} = require('electron').remote;
 var request = require('request');
 var child = require("child_process").execFile;
-var online_version
-var maindir = app.getPath("userData")
+var online_version;
+var maindir = app.getPath("userData");
 // Main internal API for the main window 
 var App = {
     disableButtons: function(){
-        buttons = document.getElementsByTagName("button")
+        buttons = document.getElementsByTagName("button");
         for(var i = 0; i < buttons.length; i++){
-            buttons[i].disabled = true
+            buttons[i].disabled = true;
         }
     },
     enableButtons: function(){
-        buttons = document.getElementsByTagName("button")
+        buttons = document.getElementsByTagName("button");
         for(var i = 0; i < buttons.length; i++){
-            buttons[i].disabled = false
+            buttons[i].disabled = false;
         }
     },
     close: function () {
@@ -33,28 +30,28 @@ var App = {
         if(process.platform == 'darwin'){
             var mc = child(path.join(maindir, "process", "MultiMC.app"),["-l","NAM Pack"], function(err, data){
                 if (err){
-                    App.statebg("lightblue")
-                    App.state("Error: could not launch MultiMC")
+                    App.statebg("lightblue");
+                    App.state("Error: could not launch MultiMC");
                     console.error(err);
                 }
             });
-            ipcRenderer.send("hide")
+            ipcRenderer.send("hide");
             mc.on('exit', (code) => {
                 console.log(`child process exited with code ${code}`);
-                App.close()
+                App.close();
             });
         }else if(process.platform == 'win32'){
             var mc = child(path.join(maindir, "process", "MultiMC", "MultiMC.exe"),["-l","NAM Pack"], function(err, data){
                 if (err){
-                    App.statebg("lightblue")
-                    App.state("Error: could not launch MultiMC")
+                    App.statebg("lightblue");
+                    App.state("Error: could not launch MultiMC");
                     console.error(err);
                 }
             });
-            ipcRenderer.send("hide")
+            ipcRenderer.send("hide");
             mc.on('exit', (code) => {
                 console.log(`child process exited with code ${code}`);
-                App.close()
+                App.close();
             });
         }
     },
@@ -62,30 +59,30 @@ var App = {
         if(process.platform == 'darwin'){
             var mc = child(path.join(maindir, "process", "MultiMC.app"), function(err, data){
                 if (err){
-                    App.statebg("lightblue")
-                    App.state("Error: could not launch MultiMC normally")
+                    App.statebg("lightblue");
+                    App.state("Error: could not launch MultiMC normally");
                     console.error(err);
                 }
                 
             });
             // TODO: fix stdout redirect
             mc.stdout.on('data', function(data) {
-                App.statebg("lightblue")
-                App.statebg(data)
-                console.log(data)
+                App.statebg("lightblue");
+                App.statebg(data);
+                console.log(data);
             });
         }else if(process.platform == 'win32'){
             var mc = child(path.join(maindir, "process", "MultiMC", "MultiMC.exe"), function(err, data){
                 if (err){
-                    App.statebg("lightblue")
-                    App.state("Error: could not launch MultiMC normally")
+                    App.statebg("lightblue");
+                    App.state("Error: could not launch MultiMC normally");
                     console.error(err);
                 }
             });
             mc.stdout.on('data', function(data) {
-                App.statebg("lightblue")
-                App.statebg(data)
-                console.log(data)
+                App.statebg("lightblue");
+                App.statebg(data);
+                console.log(data);
             });
         }
     },
@@ -93,28 +90,28 @@ var App = {
         if(process.platform == 'darwin'){
             var mc = child(path.join(maindir, "process", "MultiMC.app"),["-l","Vanilla"], function(err, data){
                 if (err){
-                    App.statebg("lightblue")
-                    App.state("Error: could not launch MultiMC")
+                    App.statebg("lightblue");
+                    App.state("Error: could not launch MultiMC");
                     console.error(err);
                 }
             });
-            ipcRenderer.send("hide")
+            ipcRenderer.send("hide");
             mc.on('exit', (code) => {
                 console.log(`child process exited with code ${code}`);
-                App.close()
+                App.close();
             });
         }else if(process.platform == 'win32'){
             var mc = child(path.join(maindir, "process", "MultiMC", "MultiMC.exe"),["-l","Vanilla"], function(err, data){
                 if (err){
-                    App.statebg("lightblue")
-                    App.state("Error: could not launch MultiMC")
+                    App.statebg("lightblue");
+                    App.state("Error: could not launch MultiMC");
                     console.error(err);
                 }
             });
-            ipcRenderer.send("hide")
+            ipcRenderer.send("hide");
             mc.on('exit', (code) => {
                 console.log(`child process exited with code ${code}`);
-                App.close()
+                App.close();
             });
         }
     },
@@ -125,21 +122,21 @@ var App = {
         document.getElementById("main-button").innerHTML = message;
     },
     state: function(message){
-        document.getElementById("state").innerText = message
+        document.getElementById("state").innerText = message;
     },
     updateInfo: function(message){
-        document.getElementById("update").innerText = message
+        document.getElementById("update").innerText = message;
     },
     update: function(){
         ipcRenderer.send("checkUpdate");
     },
     statebg: function(color){
-        document.getElementById("state").style = "background-color:" + color
+        document.getElementById("state").style = "background-color:" + color;
     },
     downloadmc: function(){
-        App.disableButtons()
-        App.statebg("transparent")
-        App.state("Downloading from official website...")
+        App.disableButtons();
+        App.statebg("transparent");
+        App.state("Downloading from official website...");
         if(process.platform == 'darwin'){
             ipcRenderer.send("download", {
                 url: "https://files.multimc.org/downloads/mmc-stable-osx64.tar.gz",
@@ -153,121 +150,121 @@ var App = {
         }
     },
     import: function(){
-        App.state("Import not implemented yet")
+        App.state("Import not implemented yet");
     },
     processDownload: function(pathname){
         if(process.platform == 'darwin'){
-            App.state("OS X support for untar is under deveopment!")
-            console.log("OS X support for untar is under deveopment!")
+            App.state("OS X support for untar is under deveopment!");
+            console.log("OS X support for untar is under deveopment!");
         }else if(process.platform == 'win32'){
             extract(pathname,{dir: path.join(maindir, "process")},function (err){
                 if(err){
-                    App.statebg("lightblue")
+                    App.statebg("lightblue");
                     App.state("There was a error extracting the downloaded file\n" + err);
-                    console.log(err)
+                    console.log(err);
                 }
                 // Delete the original downloaded file
                 fs.unlink(pathname, function (err) {
                     if (err) throw err;
                     // App reload here instead of outside of function to ensure synchronous runtime
-                    App.reload()
+                    App.reload();
                 });
-            })
-            App.state("Finished extracting, restarting...")
+            });
+            App.state("Finished extracting, restarting...");
         }
         
     },
     reload: function(){
         // if there is no error
         if(document.getElementById("state").innerText.search(/error/i) == -1)
-            getCurrentWindow().reload()
+            getCurrentWindow().reload();
     },
     vanilla: function(){
         if(process.platform == 'darwin'){
-            var mcpath = path.join(maindir, "process" ,"MultiMC.app")
+            var mcpath = path.join(maindir, "process" ,"MultiMC.app");
         }else if(process.platform == 'win32'){
-            var mcpath = path.join(maindir, "process" ,"MultiMC")
+            var mcpath = path.join(maindir, "process" ,"MultiMC");
         }
         if(fs.existsSync(path.join(mcpath, "instances", "Vanilla"))){
-            console.log("Vanilla exists")
-            var instancePath = path.join(mcpath, "instances", "Vanilla")
-            var man = require(path.join(instancePath,"manifest.json"))
+            console.log("Vanilla exists");
+            var instancePath = path.join(mcpath, "instances", "Vanilla");
+            var man = require(path.join(instancePath,"manifest.json"));
             if(online_version > man.version){
                 // Download the latest dist of Vanilla
-                App.state("Updating Vanilla to: " + online_version)
+                App.state("Updating Vanilla to: " + online_version);
                 ipcRenderer.send("vanillaNewpack");
             }else{
-                App.state("Vanilla release: " + man.version)
+                App.state("Vanilla release: " + man.version);
                 App.changeButton(false, App.launch, "Launch");
             }
         }else{
-            App.state("Downloading Vanilla")
+            App.state("Downloading Vanilla");
             ipcRenderer.send("vanillaNewpack");
         }
-        document.getElementById("vanilla").onclick = App.launchVanilla
+        document.getElementById("vanilla").onclick = App.launchVanilla;
     }
-}
+};
 
-App.state("Loading...")
-App.changeButton(true, null, "Loading...")
+App.state("Loading...");
+App.changeButton(true, null, "Loading...");
 
 // Check for application update
-App.update()
+App.update();
 
 ipcRenderer.on("latest", (event, file) => {
     // Check for existing MultiMC instance in userData
     if(process.platform == 'darwin'){
-        var mcpath = path.join(maindir, "process" ,"MultiMC.app")
+        var mcpath = path.join(maindir, "process" ,"MultiMC.app");
     }else if(process.platform == 'win32'){
-        var mcpath = path.join(maindir, "process" ,"MultiMC")
+        var mcpath = path.join(maindir, "process" ,"MultiMC");
     }
 
     if (fs.existsSync(mcpath)) {
-        App.state("MultiMC instance found")
+        App.state("MultiMC instance found");
         // Check for NAM pack update using github releses
-        var online_version
+        var online_version;
         var r = request.get('https://github.com/borisliao/nam-dist/releases/latest', function (err, res, body) {
             // extract the last numbers from the url
-            online_version = r.uri.href.substring(r.uri.href.lastIndexOf("/") + 1)
+            online_version = r.uri.href.substring(r.uri.href.lastIndexOf("/") + 1);
             // check current version of NAM Pack
-            var instancePath = path.join(mcpath, "instances", "NAM Pack")
+            var instancePath = path.join(mcpath, "instances", "NAM Pack");
             if(fs.existsSync(path.join(mcpath, "instances", "NAM Pack","minecraft"))){
-                console.log("NAM Pack exists")
-                var man = require(path.join(instancePath,"manifest.json"))
+                console.log("NAM Pack exists");
+                var man = require(path.join(instancePath,"manifest.json"));
                 if(online_version > man.version){
                     // Download the latest dist of NAM pack
-                    App.state("Updating NAM Pack to: " + online_version)
+                    App.state("Updating NAM Pack to: " + online_version);
                     ipcRenderer.send("newpack");
                 }else{
-                    App.state("NAM Pack release: " + man.version)
+                    App.state("NAM Pack release: " + man.version);
                     App.changeButton(false, App.launch, "Launch");
                 }
             }else{
-                App.state("Downloading NAM Pack")
+                App.state("Downloading NAM Pack");
                 ipcRenderer.send("newpack");
             }
 
         });
     } else {
-        App.state('MultiMC does not exist.\nWould you like to download a new MultiMC or import a old MultiMC instance?')
-        App.statebg("lightblue")
+        App.state('MultiMC does not exist.\nWould you like to download a new MultiMC or import a old MultiMC instance?');
+        App.statebg("lightblue");
         App.changeButton(false, App.downloadmc, "Download New");
     }
 
     ipcRenderer.on("download complete", (event, file) => {
-        App.state("Download finished! saved at: " + file)
-        App.processDownload(file)
+        App.state("Download finished! saved at: " + file);
+        App.processDownload(file);
         // App.enableButtons()
     });
 
     ipcRenderer.on("modpack complete", (event, file) => {
-        App.state("Download finished! saved at: " + file)
-        App.processModpack(file)
+        App.state("Download finished! saved at: " + file);
+        App.processModpack(file);
         // App.enableButtons()
     });
 
     ipcRenderer.on("download progress", (event, progress) => {
         const cleanProgressInPercentages = Math.floor(progress.percent * 100); // Without decimal point
-        App.state("Downloading: " + cleanProgressInPercentages + '%')
+        App.state("Downloading: " + cleanProgressInPercentages + '%');
     });
 });
