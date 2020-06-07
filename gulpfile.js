acca7307e1f80cd169b228012c0caf44c3b77925
Boris Liao <borisliaos@gmail.com>
2020-06-07 13:13:18 -0400

    livereload implemented

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,36 @@ const exec = require('child_process').exec;
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const css = require('gulp-clean-css');
+const livereload = require('gulp-livereload');
+
 // 1. Copy the index.html as is
 gulp.task('html', () => {
-    return gulp.src('./src/index.html')
-        .pipe(gulp.dest('app/'));
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest('app/'))
+        .pipe(livereload());
 });
 // 2. Compile CSS file and move them to the app folder
-gulp.task('css', () => { // 2.
+gulp.task('css', () => {
     return gulp.src('src/**/*.css')
         .pipe(css())
-        .pipe(gulp.dest('app/'));
+        .pipe(gulp.dest('app/'))
+        .pipe(livereload());
 });
 // 3. Compile JS files and move them to the app folder
-gulp.task('js', () => { // 3.
+gulp.task('js', () => {
     return gulp.src(['main.js', 'src/**/*.js'])
          .pipe(babel())
-         .pipe(gulp.dest('app/'));
+         .pipe(gulp.dest('app/'))
+         .pipe(livereload());
 });
+
+gulp.task('watch', () => {
+    livereload.listen();
+    gulp.watch('src/**/*.html', gulp.series('html'));
+    gulp.watch('src/**/*.css', gulp.series('css'));
+    gulp.watch('src/**/*.js', gulp.series('js'));
+  });
+
 // 4. Start the electron process.
 gulp.task('start', gulp.series('html', 'css', 'js', () => { // 4.
     return exec(
@@ -27,6 +40,8 @@ gulp.task('start', gulp.series('html', 'css', 'js', () => { // 4.
 }));
 
 gulp.task('build', gulp.series('html', 'css', 'js'));
+
+gulp.task('default', gulp.parallel('start', 'watch'));
 
 gulp.task('release', gulp.series('build', () => {
     return exec(
