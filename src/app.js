7d0bd2d25f14735d1454fb58cd74bd620f8b57cc
Boris Liao <borisliaos@gmail.com>
2020-06-07 18:41:16 -0400

    Navbar using react components

--- src/app.js
+++ src/app.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import { Container, Row, Col } from 'react-bootstrap';
+import Navbar from './gui/MainNavbar';
 
-export default class App extends React.Component {
-    render() {
-        return <div><h2>Hello Electrate</h2></div>;
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+      }
+      
+    componentDidMount() {
+        this.timerID = setInterval(
+        () => this.tick(),
+        1000
+        );
     }
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+    
+    tick() {
+        this.setState({
+        date: new Date()
+        });
+    }
+    render() {
+        return (
+        <Navbar />
+    );}
 }
