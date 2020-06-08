c7c18f87bf9ef9eba6d8c2d161a269033f8c89cf
Boris Liao <borisliaos@gmail.com>
2020-06-08 13:24:36 -0400

    Seperated information from the selector code

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Navbar from './gui/MainNavbar'
+import Info from './gui/Info'
 import Selector from './gui/Selector'
 
 export default function App () {
   return (
     <>
       <Navbar />
+      <Info />
       <Selector />
     </>
   )
