bd4afdf2e2561267b6a5715b59a9ea0468fd352f
Boris Liao <borisliaos@gmail.com>
2020-06-07 23:36:11 -0400

    Added appVersion for navbar

--- src/gui/MainNavbar.js
+++ src/gui/MainNavbar.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 
+const appVersion = window.require('electron').remote.app.getVersion()
+
 export default function MainNavbar () {
   return (
     <Navbar bg="light">
       <Navbar.Brand href="#home">
         NAM Pack
       </Navbar.Brand>
-      <Navbar.Text><em>v0.0.1</em></Navbar.Text>
+      <Navbar.Text><em>v{appVersion}</em></Navbar.Text>
     </Navbar>
   )
 };
