4429f748683cd425fab2fe8f39a060c5c9171297
Boris Liao <borisliaos@gmail.com>
2020-06-08 13:19:03 -0400

    Initial selector.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import Navbar from './gui/MainNavbar'
+import Selector from './gui/Selector'
 
 export default function App () {
   return (
     <>
       <Navbar />
+      <Selector />
     </>
   )
 }
---
+++ src/gui/Selector.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import Media from 'react-bootstrap/Media'
+import Carousel from 'react-bootstrap/Carousel'
+import { Container } from 'react-bootstrap'
+
+export default function Selector () {
+  return (
+    <Container fluid>
+      <Media>
+        <Media.Body>
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=First slide&bg=373940"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Second slide&bg=282c34"
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Third slide&bg=20232a"
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+          <h5>List-based media object</h5>
+          <p>
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+        ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+        tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+        fringilla. Donec lacinia congue felis in faucibus.
+          </p>
+        </Media.Body>
+      </Media>
+    </Container>
+  )
+}
