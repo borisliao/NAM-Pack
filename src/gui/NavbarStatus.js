0dc3ce50acc545380d3dc6d09f34d8f8009edf95
Boris Liao <borisliaos@gmail.com>
2020-06-08 16:19:53 -0400

    Added hooks

--- src/gui/NavbarStatus.js
+++ src/gui/NavbarStatus.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Spinner from 'react-bootstrap/Spinner'
 import Navbar from 'react-bootstrap/Navbar'
 import { Container, Col } from 'react-bootstrap'
 
 export default function NavbarStatus () {
+  const [loading, setLoading] = useState(true)
+  const [status, setStatus] = useState('Starting application')
+
   return (
     <Navbar.Text>
       <Container>
-        Checking for Updates
-        <Col><Spinner animation="border" size="sm" /></Col>
+        {status}
+        {loading &&
+          <Col><Spinner animation="border" size="sm" /></Col>
+        }
       </Container>
     </Navbar.Text>
   )
 };
+  
