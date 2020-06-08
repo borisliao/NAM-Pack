c77530e04b8ac58abb7c80250c4c827f50c927f0
Boris Liao <borisliaos@gmail.com>
2020-06-07 23:54:09 -0400

    Checking for Updates spinner

--- src/gui/MainNavbar.js
+++ src/gui/MainNavbar.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
+import Spinner from 'react-bootstrap/Spinner'
 
 const appVersion = window.require('electron').remote.app.getVersion()
 
 export default function MainNavbar () {
   return (
     <Navbar bg="light">
-      <Navbar.Brand href="#home">
-        NAM Pack
-      </Navbar.Brand>
-      <Navbar.Text><em>v{appVersion}</em></Navbar.Text>
+      <Navbar.Brand href="#home"> NAM Pack </Navbar.Brand>
+      <Navbar.Text><em> v{appVersion} </em></Navbar.Text>
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>
+          Checking for Updates
+          <Spinner animation="border" size="sm" />
+        </Navbar.Text>
+      </Navbar.Collapse>
     </Navbar>
   )
 };
