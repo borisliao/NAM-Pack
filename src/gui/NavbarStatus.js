b9768863c05be7128226347986955b01aa7c44ba
Boris Liao <borisliaos@gmail.com>
2020-06-09 00:11:35 -0400

    exported state to navbar in es6

--- src/gui/NavbarStatus.js
+++ src/gui/NavbarStatus.js
@@ -2,11 +2,20 @@ import React, { useState, useEffect } from 'react'
 import Spinner from 'react-bootstrap/Spinner'
 import Navbar from 'react-bootstrap/Navbar'
 import { Container, Col } from 'react-bootstrap'
+import { state } from '../index'
 
 export default function NavbarStatus () {
   const [loading, setLoading] = useState(true)
   const [status, setStatus] = useState('Starting application')
-
+  useEffect(() => {
+    function handleStatusChange(status) {
+      setStatus(status)
+    }
+    state.subscribeStatus(handleStatusChange)
+    return () => {
+      state.unsubscribeStatus(handleStatusChange);
+    }
+  })
   return (
     <Navbar.Text>
       <Container>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app.js'
+import StateAPI from '../src/api/State'
 
 window.onload = () => {
   ReactDOM.render(<App />, document.getElementById('app'))
 }
+
+// Primary application state manager
+export const state = new StateAPI()
+
+state.status = 'Loading...'
