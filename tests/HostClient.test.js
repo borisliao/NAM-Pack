fb2e9ad98e1ef5bc5394e19e3c05f52e5ce42f98
Boris Liao <borisliaos@gmail.com>
2020-06-10 00:51:13 -0400

    test file created

--- tests/HostClient.test.js
+++ tests/HostClient.test.js
@@ -1,6 +1,36 @@
 import HostClient from '../src/api/HostClient'
 
-test('loading initial state is true', () => {
+test('mainFolder is ./', () => {
   const Host = new HostClient('./')
   expect(Host.mainFolder).toBe('./')
+})
+
+test('existsInstance finds NAM Pack, method: folder', () => {
+  const Host = new HostClient('./tests/process/')
+  const x = Host.existsInstance('NAM Pack', 'folder')
+  expect(x).toBe(true)
+})
+
+test('existsInstance doesnt find NAM Pack, method: manifest.json', () => {
+  const Host = new HostClient('./tests/process/')
+  const x = Host.existsInstance('NAM Pack', 'manifest.json')
+  expect(x).toBe(false)
+})
+
+test('existsInstance find NAM Pack 2020, method: manifest.json', () => {
+  const Host = new HostClient('./tests/process/')
+  const x = Host.existsInstance('NAM Pack 2020', 'manifest.json')
+  expect(x).toBe(true)
+})
+
+test('getInstance gets all the instances', () => {
+  const Host = new HostClient('./tests/process/')
+  const x = Host.getInstances()
+  expect(x.length).toBe(2)
+  expect(x[0].name).toBe('NAM Pack 2020')
+  expect(x[0].version).toBe('5.0.2')
+  expect(x[0].folder).toBe('tests\\process\\MultiMC\\instances\\NAM Pack')
+  expect(x[1].name).toBe('Vanilla')
+  expect(x[1].version).toBe('5.0.1')
+  expect(x[1].folder).toBe('tests\\process\\MultiMC\\instances\\Vanilla')
 })
