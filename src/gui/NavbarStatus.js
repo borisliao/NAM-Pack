91a8cd8a03e2d54c23af6b67cb190c7239459dfd
Boris Liao <borisliaos@gmail.com>
2020-06-11 16:13:06 -0400

    added progress hook

--- src/gui/NavbarStatus.js
+++ src/gui/NavbarStatus.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react'
 import Spinner from 'react-bootstrap/Spinner'
 import Navbar from 'react-bootstrap/Navbar'
 import { Container, Col } from 'react-bootstrap'
+import ProgressBar from 'react-bootstrap/ProgressBar'
 
 export default function NavbarStatus () {
   const [loading, setLoading] = useState(true)
   const [status, setStatus] = useState('Starting application')
+  const [progress, setProgress] = useState(0)
   const State = window.State
 
   useEffect(() => {
@@ -17,21 +19,34 @@ export default function NavbarStatus () {
       setLoading(State.loading)
     }
 
+    function navbarHandleProgressChange (loading) {
+      // If progress == 100, reset back to 0
+      if (State.progress !== 100) {
+        setProgress(State.progress)
+      } else {
+        setProgress(0)
+      }
+    }
+
     State.subscribeStatus(navbarHandleStatusChange)
     State.subscribeLoading(navbarHandleLoadingChange)
+    State.subscribeProgress(navbarHandleProgressChange)
 
     navbarHandleStatusChange()
     navbarHandleLoadingChange()
+    navbarHandleProgressChange()
 
     return function cleanup () {
       State.unsubscribeStatus(navbarHandleStatusChange)
       State.unsubscribeLoading(navbarHandleLoadingChange)
+      State.unsubscribeProgress(navbarHandleProgressChange)
     }
   })
 
   return (
     <Navbar.Text>
       <Container>
+        {progress !== 0 && <ProgressBar now={60} />}
         {status}
         {loading &&
           <Col><Spinner animation="border" size="sm" /></Col>
