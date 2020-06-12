7ba1a1906fe26b155830b9fd7178ab996d09c8ec
Boris Liao <borisliaos@gmail.com>
2020-06-11 23:11:47 -0400

    expanded Instance to hold update information

--- src/api/Instance.js
+++ src/api/Instance.js
@@ -1,7 +1,9 @@
 export default class Instance {
-  constructor (folderPath, instName, instVersion) {
+  constructor (folderPath, instName, instVersion, remoteUrl = null, urlType = null) {
     this.folder = folderPath
     this.name = instName
     this.version = instVersion
+    this.url = remoteUrl
+    this.type = urlType
   }
 }
